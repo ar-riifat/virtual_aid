9092c18c67ce976f2f5366218d812b3d5384796d
Anisur Rahman Rifat <69327775+ar-riifat@users.noreply.github.com>
2023-02-21 03:05:25 +0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\development\\sdks\\flutter_windows_3.3.8-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\project\\med_aid" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\development\\sdks\\flutter_windows_3.3.8-stable\\flutter"
+  "PROJECT_DIR=F:\\project\\med_aid"
+  "FLUTTER_ROOT=C:\\development\\sdks\\flutter_windows_3.3.8-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\project\\med_aid\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\project\\med_aid"
+  "FLUTTER_TARGET=F:\\project\\med_aid\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\project\\med_aid\\.dart_tool\\package_config.json"
+)
