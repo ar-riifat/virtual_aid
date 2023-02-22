fea906eae09a9e2cb76d25bec445b9164211e5b7
Anisur Rahman Rifat <69327775+ar-riifat@users.noreply.github.com>
2023-02-23 01:33:13 +0600

    Add files via upload

--- lib/Blood Bank/DonorList.dart
+++ lib/Blood Bank/DonorList.dart
@@ -1,110 +1,131 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-class DonorList extends StatefulWidget{
+import 'AddDonor.dart';
+
+class DonorList extends StatefulWidget {
   @override
   State<DonorList> createState() => _DonorListState();
 }
 
 class _DonorListState extends State<DonorList> {
   late final Stream<QuerySnapshot> _DonorDetailsStream;
-@override
-Widget build(BuildContext contex){
-  return Scaffold(
-    backgroundColor: const Color.fromARGB(255, 243, 197, 193),
-    appBar: AppBar(
-        title: const Text('Blood Bank'),
+
+  @override
+  void initState() {
+    super.initState();
+    _DonorDetailsStream =
+        FirebaseFirestore.instance.collection("DonorList").snapshots();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 243, 197, 193),
+      appBar: AppBar(
+        title: const Text("Donor List"),
         backgroundColor: Colors.red.withOpacity(0.85),
       ),
-      body: Material(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 120,
-            width: MediaQuery.of(contex).size.width,
-            decoration: BoxDecoration(
-              color: Colors.green.withOpacity(0.5),
-              borderRadius: BorderRadius.circular(20)
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text("Name",
-                      style: TextStyle(
-                        fontSize:22,
-                        color: Colors.black,
-
-                      ),
-                      ),
-                      SizedBox(height:5),
-                      Text("Age",
-                      style: TextStyle(
-                        fontSize:22,
-                        color: Colors.black,
-
-                      ),
-                     
-                      ),
-                       SizedBox(height:5),
-                      Text("Phone Number",
-                      style: TextStyle(
-                        fontSize:22,
-                        color: Colors.black,
-
-                      ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 50.0),
-                    child: Text(
-                      "AB+",
-                    style: TextStyle(
-                      fontSize: 24,
-                    ),
-                    ),
-                  ),
-                ],
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _DonorDetailsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Something went wrong");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.green,
               ),
+            );
+          }
+          if (snapshot.data!.docs.isEmpty) {
+            return const Center(
+              child: Text(
+                "The List is Empty",
+                //style: TextStyle(),
+              ),
+            );
+          }
+          return Material(
+            color: const Color.fromARGB(255, 243, 197, 193),
+            child: ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 120,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: Colors.green.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                snapshot.data!.docs[index]['name'],
+                                style: const TextStyle(
+                                  fontSize: 22,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                snapshot.data!.docs[index]['age'].toString(),
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                ),
+                              ),
+                             const SizedBox(height: 5),
+                              Text(
+                                snapshot.data!.docs[index]['phonenumber'],
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          ),
+                           Padding(
+                            padding: const EdgeInsets.only(right: 50),
+                            child: Text(
+                              snapshot.data!.docs[index]['bloodgroup'],
+                              style: const TextStyle(
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          ),
-        ),
+          );
+        },
       ),
-      // body: StreamBuilder<QuerySnapshot>(
-      //   stream: _DonorDetailsStream,
-      //   builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
-      //     if(snapshot.hasError){
-      //       return const Text("Something went wrong");
-      //     }
-      //     if(snapshot.connectionState == ConnectionState.waiting){
-      //       return const Center(child: CircularProgressIndicator(
-      //         color: Colors.green
-      //       ),
-      //       );
-      //     }
-      //     if (snapshot.data!.docs.isEmpty){
-      //       return const Center(
-      //         child: Text(
-      //           "The List If Empty",
-      //         )
-      //       );
-      //     }
-
-      //     return Material(
-      //       child: ListView.builder(
-      //         itemCount: snapshot.data!.docs.length,
-      //         itemBuilder: (BuildContext contex, int index){
-      //           return Container();
-      //         },
-      //       ),
-      //     );
-      //   },
-      // )
-  );
-}
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red.withOpacity(0.9),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) {
+                return AddDonor();
+              },
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
